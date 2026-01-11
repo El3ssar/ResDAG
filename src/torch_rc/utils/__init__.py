08512d6145a8f2d1ba54af8c023e1dd7d51ad628
Daniel Estevez <kemossabee@gmail.com>
2026-01-11 22:40:06 +0100

    Remove unused forecast

--- src/torch_rc/utils/__init__.py
+++ src/torch_rc/utils/__init__.py
@@ -1,7 +1,6 @@
 """Utility functions for torch_rc."""
 
 from . import data
-from .functional_forecast import functional_esn_forecast
 from .general import create_rng
 
-__all__ = ["create_rng", "functional_esn_forecast", "data"]
+__all__ = ["create_rng", "data"]
